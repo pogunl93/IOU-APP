43cce9c063d21c3f3ce962f5223c5646aa8b322a
Paul <ogunlowo.p@gmail.com>
2013-07-24 00:18:23 -0400

    Added the signup, login and signout functionalities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130723235825) do
+ActiveRecord::Schema.define(version: 20130724005819) do
 
   create_table "ious", force: true do |t|
     t.integer "owner_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20130723235825) do
     t.string "fname"
     t.string "lname"
     t.string "email"
+    t.string "password"
   end
 
 end
--- iouapp.rb
+++ iouapp.rb
@@ -5,14 +5,59 @@ require 'sinatra/activerecord'
 require 'scoped_search'
 require 'stringio'
 configure(:development){ set :database, "sqlite3:///Twitter_DB.sqlite3" }
-#require './models'
+require './models'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 require 'bundler/setup'
 require 'sinatra/base'
 require 'rack-flash'
 require './models'
+require './helpers'
+
+enable :sessions 
+use Rack::Flash, :sweep => true 
+
+set :sessions => true 
 
 get '/' do 
 	haml :home
+end 
+
+get '/sign_up' do
+	haml :sign_up
+end 
+
+post '/sign_up' do
+	if params[:password] == params[:confirm_password]
+		User.create(:fname => params[:fname], :lname => params[:lname], :email => params[:email], :password => params[:password])
+	else 
+		flash[:notice] = "Your passwords do not match"
+	end 
+	redirect '/sign_up'
+end 
+
+get '/sign_in' do 
+	haml :sign_in
+end 
+
+post '/sign_in' do 
+	@user = User.where(:email => params[:email]).first
+	if @user 
+		if @user.password == params[:password]
+			session[:user_id] = @user.id
+			flash[:notice] = "Welcome back, #{@user.fname}"
+			redirect '/'
+		else 
+			flash[:notice] = "Your password was wrong, please try again"
+			redirect '/sign_in'
+		end 
+	else 
+		flash[:notice] = "Your username was not found"
+		redirect '/sign_in'
+	end 
+end
+
+get '/sign_out' do 
+	session[:user_id] = nil
+	redirect '/'
 end 
