84f446badcd80bc74e62ea106b90ee0f2ed9e83b
Alexander Luce <alexluce@gmail.com>
2013-07-25 19:27:21 -0400

    Changed profile to include totals

--- helpers.rb
+++ helpers.rb
@@ -2,4 +2,12 @@ helpers do
 	def current_user
 		session[:user_id].nil? ? nil : User.find(session[:user_id])
 	end 
+
+  def totals(ious)
+    total = 0
+    ious.each do |iou|
+      total = total + iou.amount
+    end
+    total
+  end
 end 
